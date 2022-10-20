f7ed3de91b18bc1b20f60d058112a7f8de6c4b07
Amdadul Haque <amdadul35500@gmail.com>
2022-10-20 17:05:28 +0600

    typescript shopping cart

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
